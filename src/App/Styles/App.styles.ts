e24b64a9f2417d11525122e66001dac940bcc417
carloshenriquemkt <falacom.ocarlos@gmail.com>
2022-06-25 22:25:00 -0300

    smooth scroll

--- src/App/Styles/App.styles.ts
+++ src/App/Styles/App.styles.ts
@@ -1,22 +1,19 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from "styled-components";
 
-export const Container = styled.div` /* My tag body have a default 8px margin that I don't know how to delete */
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    
-    font-family: sans-serif;
+export const Container = styled.div`
+	/* My tag body have a default 8px margin that I don't know how to delete */
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+
+	font-family: sans-serif;
 `;
 
 export const MyGlobalStyle = createGlobalStyle`
   body, html {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;;
+    box-sizing: border-box;
+    scroll-behavior: smooth;
   }
 `;
-
-
-
-
-
